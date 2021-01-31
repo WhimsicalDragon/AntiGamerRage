5a0693123689175f20c9f627919474f453decaa0
WhimsicalDragon <gabrielcablue@gmail.com>
2021-01-31 06:46:12 +0000

    Opens some relaxing nature sounds when you get too loud and are not alright

--- GUI.vbs
+++ GUI.vbs
@@ -1,8 +1,16 @@
+Dim FSO
+Set FSO = CreateObject("Scripting.FileSystemObject")
+
 intAnswer = _
     Msgbox("Whoa gamer you're being kinda loud there. Are you ok?", _
         vbYesNo, "Are you ok")
 If intAnswer = vbYes Then
-    Msgbox "You answered yes."
+    Set OutPutFile = FSO.OpenTextFile("answer.txt" ,2 , True)
+    OutPutFile.WriteLine("Yes")
+    Set FSO= Nothing
+
 Else
-    Msgbox "You answered no."
+    Set OutPutFile = FSO.OpenTextFile("answer.txt" ,2 , True)
+    OutPutFile.WriteLine("No")
+    Set FSO= Nothing
 End If
--- antiGamerRage.py
+++ antiGamerRage.py
@@ -1,24 +1,29 @@
 import sounddevice as sd
 import numpy as np
+import os
+import subprocess
 
 global stopStream
 stopStream = 0
-
+os.chdir("Cornhacks")
 def print_sound(indata, outdata, frames, time, status):
     global stopStream
     volume_norm = np.linalg.norm(indata)*10
-    #print ("|" * int(volume_norm))
     print(volume_norm)
 
     if volume_norm > 40 :
         print("Loud gamer!")
         stopStream = 1
+        os.system("cd")
+        os.system("cscript.exe GUI.vbs")
 
+        f = open("answer.txt", "r")
+        if f.read(5) == "No\n" :
+            os.system("start firefox.exe https://www.youtube.com/watch?v=V1RPi2MYptM")
 
-#while stopStream == 0:
 with sd.Stream(callback=print_sound):
     pass
-    while stopStream == 0:
+    while True :
         pass
     sd.sleep(100)
 
